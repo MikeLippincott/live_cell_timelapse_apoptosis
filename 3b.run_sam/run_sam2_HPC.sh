1cbdc8d892684335ccaa861662e5d31902544db3
MikeLippincott <1michaell2017@gmail.com>
2024-10-10 15:02:54 -0600

    find best gpu combos

--- 3b.run_sam/run_sam2_HPC.sh
+++ 3b.run_sam/run_sam2_HPC.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --mem=100G
-#SBATCH --time=6:00:00
-#SBATCH --partition=aa100
-#SBATCH --gres=gpu:1
-#SBATCH --output=alpine_std_out_std_err-%j.out
-#SBATCH --array=1-20
-
 # run SAM2
 echo "Starting SAM2 pipe for object detection..."
 
@@ -18,21 +10,8 @@ module load mambaforge
 conda init bash
 mamba activate sam2_env
 
-models=( "tiny" "small" "base" "large" )
-downscale_factors=( 2 4 5 10 19 )
-
-# get the number of jobs for the array
-n_models=${#models[@]}
-n_downscale_factors=${#downscale_factors[@]}
-n_jobs=$((n_models * n_downscale_factors))
-
-# get the model index
-model_index=$((($SLURM_ARRAY_TASK_ID-1) / $n_downscale_factors))
-downscale_index=$((($SLURM_ARRAY_TASK_ID-1) % $n_downscale_factors))
-
-model=${models[$model_index]}
-downscale_factor=${downscale_factors[$downscale_index]}
-
+model=$1
+downscale_factor=$2
 
 jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
 cd scripts/
@@ -40,9 +19,7 @@ cd scripts/
 # run the pipelines
 python 0.create_db_for_pipe.py
 
-command="python 1.run_sam2_microscopy.py"
-
-$command --model_to_use "$model" --downscale "True" --downscale_factor "$downscale_factor"
+python 1.run_sam2_microscopy.py --model_to_use "$model" --downscale "True" --downscale_factor "$downscale_factor"
 
 cd ../../
 
