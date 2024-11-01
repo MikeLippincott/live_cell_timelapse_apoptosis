b4e871b7988343a494cc2be16194cd3c1a7ef33a
MikeLippincott <1michaell2017@gmail.com>
2024-11-01 10:38:35 -0600

    add stardist ground truth

---
+++ 3b.run_sam/run_stardist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# run SAM2
+echo "Starting stardist pipe for object detection..."
+
+downscale_factors=( 1 2 4 5 10 )
+
+mamba activate sam2_env
+
+jupyter nbconvert --to python --output-dir=scripts/ notebooks/*.ipynb
+cd scripts/ || exit
+
+for downscale_factor in "${downscale_factors[@]}"; do
+    echo "Running stardist pipe for downscale factor: $downscale_factor"
+    python 1.prepare_stardist.py --downscale_factor "$downscale_factor"
+done
+
+
+cd ../ || exit
+
+mamba deactivate
