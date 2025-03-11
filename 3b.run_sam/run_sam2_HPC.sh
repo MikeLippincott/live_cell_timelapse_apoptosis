a0a84c1d47406bf3df3428d14f8d8c716b1b27f1
MikeLippincott <1michaell2017@gmail.com>
2025-03-10 20:39:40 -0600

    test on HPC

--- 3b.run_sam/run_sam2_HPC.sh
+++ 3b.run_sam/run_sam2_HPC.sh
@@ -6,10 +6,10 @@ echo "Starting SAM2 pipe for object detection..."
 # run SAM2
 module load cuda
 module load anaconda
-module load mambaforge
+module load miniforge
 conda init bash
-mamba activate sam2_env
 
+mamba activate stardist_env
 model=$1
 downscale_factor=$2
 
@@ -18,6 +18,10 @@ cd scripts/ || exit
 
 # run the pipelines
 python 1.run_stardist.py
+
+mamba deavtivate
+
+mamba activate sam2_env
 python 2.run_sam2_microscopy.py --model_to_use "$model" --downscale_factor "$downscale_factor" --downscale
 
 
