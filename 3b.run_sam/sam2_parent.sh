8f3ee8fed6b85a4016ee37a0133c4d438fad19ab
MikeLippincott <1michaell2017@gmail.com>
2025-03-20 09:40:59 -0600

    run on HPCs 80GB vRAM

--- 3b.run_sam/sam2_parent.sh
+++ 3b.run_sam/sam2_parent.sh
@@ -20,17 +20,11 @@ for model in "${models[@]}" ; do
         while num_jobs -gt 6; do
             sleep 60
             num_jobs=$(squeue -u $USER -p aa100 | wc -l )
-	    num_jobs=$(squeue -u $USER -p al40 | wc -l) + $num_jobs
             num_jobs=$((num_jobs-1))
         done
+        sbatch --nodes=1 --ntasks=8 --mem=240G --time=22:00:00 --partition=aa100 --gres=gpu:1 --constraint=gpu80 --output=alpine_std_out_std_err-%j.out run_sam2_HPC.sh "$model" "$downscale_factor"
+        echo "Submitted job for downscale factor $downscale_factor with model $model on the 80 GPU partition."
 
-        if [ "$downscale_factor" -lt 10 ]; then
-            sbatch --nodes=1 --ntasks=8 --mem=12G --time=2:00:00 --partition=aa100 --gres=gpu:1 --constraint=gpu80 --output=alpine_std_out_std_err-%j.out run_sam2_HPC.sh "$model" "$downscale_factor"
-            echo "Submitted job for downscale factor $downscale_factor with model $model on the 80 GPU partition."
-        else
-            sbatch --nodes=1 --ntasks=8 --mem=12G --time=2:00:00 --partition=al40 --gres=gpu:1 --output=alpine_std_out_std_err-%j.out run_sam2_HPC.sh "$model" "$downscale_factor"
-            echo "Submitted job for downscale factor $downscale_factor with model $model on the 40 GPU partition."
-        fi
     done
 done
 
