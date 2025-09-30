c67f9d7c1dae1909cb1c14d6d1db282be40c9b73
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2025-09-30 15:38:43 -0600

    Offsets plot (#41)
    * precommit
    
    * adding pizaz to the plot
    
    * adding script
    
    * addressing comments

--- utils/r_themes.r
+++ utils/r_themes.r
@@ -1,4 +1,4 @@
-color_palette <- c(
+color_palette_dose <- c(
     "0" = "#132B08", #132B08
     "0.61" = "#265A0C", #265A0C
     "1.22" = "#398E0B", #398E0B
@@ -10,3 +10,36 @@ color_palette <- c(
     "78.13" = "#410C5A", #410C5A
     "156.25" = "#21082B" #21082B
 )
+
+dose_guides_color <- guides(
+        color = guide_legend(
+            override.aes = list(size = 7, alpha = 1),
+            title.position = "top",
+            title.hjust = 0.5,
+            )
+    )
+shuffle_guides_shape <- guides(
+        shape = guide_legend(
+            override.aes = list(size = 7, alpha = 1),
+            title.position = "top",
+            title.hjust = 0.5,
+            nrow = 2,
+        )
+    )
+font_size <- 24
+plot_themes <- (
+    theme_bw()
+    + theme(
+        legend.position = "bottom",
+        legend.text = element_text(size = font_size),
+        legend.title = element_text(size = font_size),
+        axis.title.x = element_text(size = font_size),
+        axis.text.x = element_text(size = font_size),
+        axis.title.y = element_text(size = font_size),
+        axis.text.y = element_text(size = font_size),
+        strip.text = element_text(size = font_size -2),
+    )
+
+
+)
+
